63de7cdec7d0e6cc6e7f9db2667a08ac095b8950
Philipp Hadjimina <p.m.hadjimina@gmail.com>
2021-09-09 10:14:15 +0200

    added smaller scripts

---
+++ misc/scripts/get_avg_comment_size.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+sys.path.insert(0,'../..')
+import constants as CS
+
+df = pd.read_csv(CS.COMMENTS_RAW)
+result = pd.DataFrame([[]])
+avg_comment_text_size = df["comment_text"].apply(lambda x: len(str(x))).mean()
+print(avg_comment_text_size)
--- misc/scripts/post_distribution_years.py
+++ misc/scripts/post_distribution_years.py
@@ -1,11 +1,29 @@
 import pandas as pd
 from datetime import datetime
 import collections
+import sys
+sys.path.insert(0,'../..')
 import constants
+
 
 df = pd.read_csv(constants.POSTS_CLEAN)
 utc = df['post_created_utc'].tolist()
 utc = [x for x in utc if str(x) != 'nan']
 utc_date = list(map(lambda x: datetime.fromtimestamp(x).year, utc))
 occurrences = collections.Counter(utc_date)
+
+# Get values in percentages
+occurrences_values = list(occurrences.values())
+occurrences_keys = list(occurrences.keys())
+
+total_posts = sum(occurrences_values)
+fn = lambda x: round(x/total_posts,2)
+
+perc_values = list(map(fn,occurrences_values))
+zip_iterator = zip(occurrences_keys, perc_values)
+perc_dict = dict(zip_iterator)
+
+print("Absolute values")
 print(occurrences)
+print("Percentages")
+print(perc_dict)
